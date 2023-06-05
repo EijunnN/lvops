4d1745d7a1417682ed09e41e274853bc314cd10d
Yoto <yoto@dev.com>
2023-06-05 15:15:56 -0500

    gaaa crmre

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+
+function toggleDarkMode() {
+  const darkModeBtn = document.getElementById('dark-mode-btn');
+  const darkModeIcon = document.getElementById('dark-mode-icon');
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  const searchForm = document.getElementsByClassName('search-form')[0];
+  darkModeBtn.classList.toggle('active');
+  
+  if (!isDarkMode) {
+    darkModeIcon.src = './dark-light/icono_light.png';
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'true');
+    searchForm.style.boxShadow = '0 1px 8px rgba(57, 213, 45, 0.5)';
+  } else {
+    darkModeIcon.src = './dark-light/icono_dark.png';
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'false');
+    searchForm.style.boxShadow = '0 1px 8px rgba(0, 0, 0, 0.3)';
+  }
+
+  // Agregar fondo blanco al icono de modo oscuro
+  darkModeIcon.style.backgroundColor = !isDarkMode ? '#fff' : 'transparent';
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  const darkMode = localStorage.getItem('darkMode');
+  const darkModeBtn = document.getElementById('dark-mode-btn');
+  const darkModeIcon = document.getElementById('dark-mode-icon');
+  const searchForm = document.getElementsByClassName('search-form')[0];
+  
+  
+  if (darkMode === 'true') {
+    darkModeBtn.classList.add('active');
+    darkModeIcon.src = './dark-light/icono_light.png';
+    document.body.classList.add('dark-mode');
+    darkModeIcon.style.backgroundColor = '#fff'; // Aplicar fondo blanco
+    searchForm.style.boxShadow = '0 1px 8px rgba(57, 213, 45, 0.5)';
+  } else {
+    darkModeBtn.classList.remove('active');
+    darkModeIcon.src = './dark-light/icono_dark.png';
+    document.body.classList.remove('dark-mode');
+    darkModeIcon.style.backgroundColor = 'transparent'; // Aplicar fondo transparente
+    searchForm.style.boxShadow = '0 1px 8px rgba(0, 0, 0, 0.3)';
+  }
+});
+
 searchTrackcode = function(){
     let trackcode = document.getElementById('search-input').value;
     if (trackcode.length > 0) {
@@ -11,17 +57,17 @@ searchTrackcode = function(){
 
 
 
-function toggleDarkMode() {
-    const darkModeBtn = document.getElementById('dark-mode-btn');
-    const darkModeIcon = document.getElementById('dark-mode-icon');
+// function toggleDarkMode() {
+//     const darkModeBtn = document.getElementById('dark-mode-btn');
+//     const darkModeIcon = document.getElementById('dark-mode-icon');
     
-    darkModeBtn.classList.toggle('active');
+//     darkModeBtn.classList.toggle('active');
     
-    if (darkModeBtn.classList.contains('active')) {
-      darkModeIcon.src = './dark-light/icono_light.png';
-      document.body.classList.add('dark-mode');
-    } else {
-      darkModeIcon.src = './dark-light/icono_dark.png';
-      document.body.classList.remove('dark-mode');
-    }
-  }
+//     if (darkModeBtn.classList.contains('active')) {
+//       darkModeIcon.src = './dark-light/icono_light.png';
+//       document.body.classList.add('dark-mode');
+//     } else {
+//       darkModeIcon.src = './dark-light/icono_dark.png';
+//       document.body.classList.remove('dark-mode');
+//     }
+//   }
